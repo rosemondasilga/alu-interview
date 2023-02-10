f223288e15b9bacc2f542c0603e23e9eb88e7a9a
rosemondasilga <rosemondasilga@gmail.com>
2023-02-10 13:13:54 +0000

    w

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
+"""
+minimum operations
+"""
 
-"""Minimum Operation"""
 
 def minOperations(n):
+    """
+   In a text file, there is a single character H
+   Your text editor can execute only two operations in this file
+    """
 
-    """In a text file, there is a single character H. Your text editor can execute only two operations in this file"""
-
-    if n <= 2:
+    if n <= 1:
         return 0
 
-    
-    for r in range(2, n/2):
-        while n % r == 2:
-            operations += r 
-            n = n / r
-    return 0
+    for t in range(2, int((n/2)+1)):
+        if n % t == 0:
+            return minOperations(int(n/t)) + t
+    return n
